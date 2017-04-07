242815a4a08d0ac345efe9845793206802b7bb5a
Olena Pastushenko <olena@MacBook-Pro-Olena.local>
2017-04-07 19:43:21 +0200

    more work for prod

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -4,19 +4,18 @@
     <h1>{{ msg }}</h1>
     <h2>Graphs list</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <li><a href="#sparklines">Sparklines</a></li>
+      <li><a href="#linecharts">Line chart</a></li>
+      <li><a href="#barcharts">Bar chart</a></li>
+      <li><a href="#bulletcharts">Bullet chart</a></li>
     </ul>
     <h2>Ecosystem</h2>
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
-    <sparklines></sparklines>
+    <sparklines name="sparklines"></sparklines>
   </div>
 </template>
 
